9a7b5e81c2fa51cf4e89657222789b58b54d03f3
Magali Torres <Tmagali776gmail.com>
2026-06-25 22:26:22 -0300

    feat: agrega clases service.cs controller.cs repository.cs

--- Agraria.Application/Interfaces/ILocalidadService.cs
+++ Agraria.Application/Interfaces/ILocalidadService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Agraria.Domain.Interfaces;
 using Agraria.Domain.Models;
 
 namespace Agraria.Application
@@ -9,4 +10,9 @@ namespace Agraria.Application
     {
         Task<List<Localidad>> GetLocalidades(CancellationToken cancellationToken = default);
     }
+    public interface IService
+    {
+        Task<List<Practicas>> GetPracticas(CancellationToken cancellationToken = default);
+    }
 }
+
---
+++ Agraria.Controller/Controllers/Controller.cs
@@ -0,0 +1,37 @@
+﻿using Agraria.Application;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Agraria.Domain.Models;
+using Agraria.Domain.Interfaces;
+
+namespace Agraria.Controller.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        private readonly IService _service;
+
+        public Controller(IService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Practicas>>> Get(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var practicas = await _service.GetPracticas(cancellationToken);
+                return Ok(practicas);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
