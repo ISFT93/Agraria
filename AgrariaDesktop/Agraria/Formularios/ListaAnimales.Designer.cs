8d46061ba82108736445415c1f3e457f018cebb0
Manucp019 <manuelaceballospardo@gmail.com>
2026-09-25 23:06:47 -0300

    arreglos e insert

--- AgrariaDesktop/Agraria/Formularios/ListaAnimales.Designer.cs
+++ AgrariaDesktop/Agraria/Formularios/ListaAnimales.Designer.cs
@@ -31,8 +31,9 @@
             components = new System.ComponentModel.Container();
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+
             timer1 = new System.Windows.Forms.Timer(components);
-            panel = new Panel();
+            panel1 = new Panel();
             BtnBuscar = new Button();
             CbFiltrar = new ComboBox();
             label1 = new Label();
@@ -40,33 +41,36 @@
             TxtNombre = new TextBox();
             LblBuscar = new Label();
             label9 = new Label();
-            panel1 = new Panel();
+            panel2 = new Panel();
             DtgAnimal = new DataGridView();
             panel3 = new Panel();
             BtnImprimir = new Button();
             BtnModificar = new Button();
             BtnSalir = new Button();
             BtnNuevo = new Button();
-            panel.SuspendLayout();
+
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)DtgAnimal).BeginInit();
             panel3.SuspendLayout();
             SuspendLayout();
+
             // 
-            // panel
+            // panel1
             // 
-            panel.Controls.Add(BtnBuscar);
-            panel.Controls.Add(CbFiltrar);
-            panel.Controls.Add(label1);
-            panel.Controls.Add(label2);
-            panel.Controls.Add(TxtNombre);
-            panel.Controls.Add(LblBuscar);
-            panel.Controls.Add(label9);
-            panel.Dock = DockStyle.Top;
-            panel.Location = new Point(0, 0);
-            panel.Name = "panel";
-            panel.Size = new Size(850, 132);
-            panel.TabIndex = 22;
+            panel1.Controls.Add(BtnBuscar);
+            panel1.Controls.Add(CbFiltrar);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(TxtNombre);
+            panel1.Controls.Add(LblBuscar);
+            panel1.Controls.Add(label9);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(850, 132);
+            panel1.TabIndex = 22;
+
             // 
             // BtnBuscar
             // 
@@ -78,6 +82,7 @@
             BtnBuscar.Text = "&Buscar";
             BtnBuscar.UseVisualStyleBackColor = true;
             BtnBuscar.Click += BtnBuscar_Click;
+
             // 
             // CbFiltrar
             // 
@@ -87,6 +92,7 @@
             CbFiltrar.Name = "CbFiltrar";
             CbFiltrar.Size = new Size(187, 32);
             CbFiltrar.TabIndex = 35;
+
             // 
             // label1
             // 
@@ -98,6 +104,7 @@
             label1.Size = new Size(106, 24);
             label1.TabIndex = 34;
             label1.Text = "Filtrar por:";
+
             // 
             // label2
             // 
@@ -106,6 +113,7 @@
             label2.Name = "label2";
             label2.Size = new Size(0, 15);
             label2.TabIndex = 33;
+
             // 
             // TxtNombre
             // 
@@ -114,6 +122,7 @@
             TxtNombre.Name = "TxtNombre";
             TxtNombre.Size = new Size(232, 29);
             TxtNombre.TabIndex = 32;
+
             // 
             // LblBuscar
             // 
@@ -125,6 +134,7 @@
             LblBuscar.Size = new Size(91, 24);
             LblBuscar.TabIndex = 31;
             LblBuscar.Text = "Nombre:";
+
             // 
             // label9
             // 
@@ -133,16 +143,79 @@
             label9.Name = "label9";
             label9.Size = new Size(0, 15);
             label9.TabIndex = 29;
+
             // 
-            // panel1
+            // panel3
+            // 
+            panel3.Controls.Add(BtnImprimir);
+            panel3.Controls.Add(BtnModificar);
+            panel3.Controls.Add(BtnSalir);
+            panel3.Controls.Add(BtnNuevo);
+            panel3.Dock = DockStyle.Right;
+            panel3.Location = new Point(719, 132);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(131, 248);
+            panel3.TabIndex = 24;
+
+            // 
+            // BtnImprimir
+            // 
+            BtnImprimir.Location = new Point(31, 61);
+            BtnImprimir.Margin = new Padding(3, 2, 3, 2);
+            BtnImprimir.Name = "BtnImprimir";
+            BtnImprimir.Size = new Size(75, 23);
+            BtnImprimir.TabIndex = 46;
+            BtnImprimir.Text = "Im&primir";
+            BtnImprimir.UseVisualStyleBackColor = true;
+            BtnImprimir.Click += BtnImprimir_Click_1;
+
+            // 
+            // BtnModificar
+            // 
+            BtnModificar.Location = new Point(31, 33);
+            BtnModificar.Margin = new Padding(3, 2, 3, 2);
+            BtnModificar.Name = "BtnModificar";
+            BtnModificar.Size = new Size(75, 23);
+            BtnModificar.TabIndex = 46;
+            BtnModificar.Text = "&Modificar";
+            BtnModificar.UseVisualStyleBackColor = true;
+            BtnModificar.Click += BtnModificar_Click_1;
+
+            // 
+            // BtnSalir
+            // 
+            BtnSalir.Location = new Point(31, 200);
+            BtnSalir.Margin = new Padding(3, 2, 3, 2);
+            BtnSalir.Name = "BtnSalir";
+            BtnSalir.Size = new Size(75, 23);
+            BtnSalir.TabIndex = 46;
+            BtnSalir.Text = "&Salir";
+            BtnSalir.UseVisualStyleBackColor = true;
+            BtnSalir.Click += BtnSalir_Click_1;
+
+            // 
+            // BtnNuevo
+            // 
+            BtnNuevo.Location = new Point(31, 5);
+            BtnNuevo.Margin = new Padding(3, 2, 3, 2);
+            BtnNuevo.Name = "BtnNuevo";
+            BtnNuevo.Size = new Size(75, 23);
+            BtnNuevo.TabIndex = 46;
+            BtnNuevo.Text = "&Nuevo";
+            BtnNuevo.UseVisualStyleBackColor = true;
+            BtnNuevo.Click += BtnNuevo_Click_1;
+
+            // 
+            // panel2
             // 
             panel2.Controls.Add(DtgAnimal);
             panel2.Dock = DockStyle.Fill;
-            panel2.Location = new Point(0, 152);
+            panel2.Location = new Point(0, 132);
             panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(972, 354);
+            panel2.Size = new Size(719, 248);
             panel2.TabIndex = 23;
+
             // 
             // DtgAnimal
             // 
@@ -174,95 +247,42 @@
             DtgAnimal.ReadOnly = true;
             DtgAnimal.RowHeadersWidth = 51;
             DtgAnimal.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            DtgAnimal.Size = new Size(972, 354);
+            DtgAnimal.Size = new Size(719, 248);
             DtgAnimal.TabIndex = 26;
+
             // 
-            // panel3
-            // 
-            panel3.Controls.Add(BtnImprimir);
-            panel3.Controls.Add(BtnModificar);
-            panel3.Controls.Add(BtnSalir);
-            panel3.Controls.Add(BtnNuevo);
-            panel3.Dock = DockStyle.Right;
-            panel3.Location = new Point(719, 132);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(131, 248);
-            panel3.TabIndex = 24;
-            // 
-            // BtnImprimir
-            // 
-            BtnImprimir.Location = new Point(31, 61);
-            BtnImprimir.Margin = new Padding(3, 2, 3, 2);
-            BtnImprimir.Name = "BtnImprimir";
-            BtnImprimir.Size = new Size(75, 23);
-            BtnImprimir.TabIndex = 46;
-            BtnImprimir.Text = "Im&primir";
-            BtnImprimir.UseVisualStyleBackColor = true;
-            BtnImprimir.Click += BtnImprimir_Click_1;
-            // 
-            // BtnModificar
-            // 
-            BtnModificar.Location = new Point(31, 33);
-            BtnModificar.Margin = new Padding(3, 2, 3, 2);
-            BtnModificar.Name = "BtnModificar";
-            BtnModificar.Size = new Size(75, 23);
-            BtnModificar.TabIndex = 46;
-            BtnModificar.Text = "&Modificar";
-            BtnModificar.UseVisualStyleBackColor = true;
-            BtnModificar.Click += BtnModificar_Click_1;
-            // 
-            // BtnSalir
-            // 
-            BtnSalir.Location = new Point(31, 233);
-            BtnSalir.Margin = new Padding(3, 2, 3, 2);
-            BtnSalir.Name = "BtnSalir";
-            BtnSalir.Size = new Size(75, 23);
-            BtnSalir.TabIndex = 46;
-            BtnSalir.Text = "&Salir";
-            BtnSalir.UseVisualStyleBackColor = true;
-            BtnSalir.Click += BtnSalir_Click_1;
-            // 
-            // BtnNuevo
-            // 
-            BtnNuevo.Location = new Point(31, 5);
-            BtnNuevo.Margin = new Padding(3, 2, 3, 2);
-            BtnNuevo.Name = "BtnNuevo";
-            BtnNuevo.Size = new Size(75, 23);
-            BtnNuevo.TabIndex = 46;
-            BtnNuevo.Text = "&Nuevo";
-            BtnNuevo.UseVisualStyleBackColor = true;
-            BtnNuevo.Click += BtnNuevo_Click_1;
-            // 
-            // FormAnimal
+            // ListaAnimales
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(141, 181, 146);
             ClientSize = new Size(850, 380);
+            Controls.Add(panel2);
             Controls.Add(panel3);
             Controls.Add(panel1);
-            Controls.Add(panel);
-            Name = "FormAnimal";
+            Name = "ListaAnimales";
             Text = "Reino Animal";
             Load += FormAnimal_Load;
-            panel.ResumeLayout(false);
-            panel.PerformLayout();
+
             panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)DtgAnimal).EndInit();
             panel3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
         private System.Windows.Forms.Timer timer1;
-        private Panel panel;
+        private Panel panel1;
         private ComboBox CbFiltrar;
         private Label label1;
         private Label label2;
         private TextBox TxtNombre;
         private Label LblBuscar;
         private Label label9;
-        private Panel panel1;
+        private Panel panel2;
         private Panel panel3;
         private Button BtnBuscar;
         private Button BtnNuevo;
